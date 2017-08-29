004e8c4bbbd8a788d3aee506a6d49cbbc5850c49
Mishin Ivan <mishin@n-core.ru>
2017-08-29 16:23:22 +0300

    [master] add start show

--- js/app.js
+++ js/app.js
@@ -1,34 +1,39 @@
 var main = function() {
+  var color = function(){
+    if (((Math.random() * 10) & 1) == 1){
+      return 'orange'
+    }else{
+      return 'black'
+    }
+  }
   var init_table = function(size) {
     var array = [];
     for (var j = 0; j < size; j++) {
       array[j] = []
       for (var i = 0; i < size; i++) {
         array[j].push(
-          {
-            object:
-              React.createElement('td', {
-                style: {
-                  backgroundColor: 'orange'
-                },
-                'data-test': '123'
-              }, '.'),
-            state: 'orange'
-          }
+          React.createElement('td', {
+            style: {
+              backgroundColor: color()
+            },
+            'data-test': '123'
+          }, '.')
         )
       }
     }
+    return array;
   }
-  return array;
+  var loop = function(){
+    var trs = []
+    init_table(10).forEach(function(curr){
+      trs.push(React.createElement('tr', null, ...curr))
+    })
+    ReactDOM.render(
+      React.createElement('tbody', null, ...trs),
+      document.getElementById('root')
+    )
+    setTimeout(loop, 500)
+  }
+  loop()
 }
-var start = init_table(10)
-console.log(start)
-  // trs.push(React.createElement('tr', null, ...tds))
-  // tds = []
-  // ReactDOM.render(
-  //   React.createElement('tbody', null, ...trs),
-  //   document.getElementById('root')
-  // )
-  // setTimeout(loop, 1000)
-};
 main()
