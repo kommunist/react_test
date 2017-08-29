77dd41c95e32000fd84504cbec4bb13565711f63
Mishin Ivan <mishin@n-core.ru>
2017-08-29 17:32:52 +0300

    [master] return to manual

--- js/app.js
+++ js/app.js
@@ -1,39 +1,36 @@
-var main = function() {
-  var color = function(){
-    if (((Math.random() * 10) & 1) == 1){
-      return 'orange'
-    }else{
-      return 'black'
-    }
+var News = React.createClass({
+  render: function() {
+    return (
+      <div className="news">
+        К сожалению, новостей нет.
+      </div>
+    );
   }
-  var init_table = function(size) {
-    var array = [];
-    for (var j = 0; j < size; j++) {
-      array[j] = []
-      for (var i = 0; i < size; i++) {
-        array[j].push(
-          React.createElement('td', {
-            style: {
-              backgroundColor: color()
-            },
-            'data-test': '123'
-          }, '.')
-        )
-      }
-    }
-    return array;
+});
+
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        Нет новостей - комментировать нечего
+      </div>
+    );
   }
-  var loop = function(){
-    var trs = []
-    init_table(10).forEach(function(curr){
-      trs.push(React.createElement('tr', null, ...curr))
-    })
-    ReactDOM.render(
-      React.createElement('tbody', null, ...trs),
-      document.getElementById('root')
-    )
-    setTimeout(loop, 500)
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Всем привет, я компонент App!
+        <News />
+        <Comments />
+      </div>
+    );
   }
-  loop()
-}
-main()
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ js/live.js
@@ -0,0 +1,39 @@
+var main = function() {
+  var color = function(){
+    if (((Math.random() * 10) & 1) == 1){
+      return 'orange'
+    }else{
+      return 'black'
+    }
+  }
+  var init_table = function(size) {
+    var array = [];
+    for (var j = 0; j < size; j++) {
+      array[j] = []
+      for (var i = 0; i < size; i++) {
+        array[j].push(
+          React.createElement('td', {
+            style: {
+              backgroundColor: color()
+            },
+            'data-test': '123'
+          }, '.')
+        )
+      }
+    }
+    return array;
+  }
+  var loop = function(){
+    var trs = []
+    init_table(10).forEach(function(curr){
+      trs.push(React.createElement('tr', null, ...curr))
+    })
+    ReactDOM.render(
+      React.createElement('tbody', null, ...trs),
+      document.getElementById('root')
+    )
+    setTimeout(loop, 300)
+  }
+  loop()
+}
+main()
