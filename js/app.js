8fbc4edad30d4f9dba3a4059bad7308a675dccc7
Mishin Ivan <mishin@n-core.ru>
2017-08-29 15:42:42 +0300

    [master] add change

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
 var main = function() {
-  var loop = function() {
-    var size = 50;
-    var tds = [];
-    var trs = [];
+  var init_table = function(size) {
+    var array = [];
     for (var j = 0; j < size; j++) {
+      array[j] = []
       for (var i = 0; i < size; i++) {
-        tds.push(React.createElement('td', {
-          style: {
-            backgroundColor: 'orange'
+        array[j].push(
+          {
+            object:
+              React.createElement('td', {
+                style: {
+                  backgroundColor: 'orange'
+                },
+                'data-test': '123'
+              }, '.'),
+            state: 'orange'
           }
-        }, '.'))
+        )
       }
-      trs.push(React.createElement('tr', null, ...tds))
-      tds = []
     }
-    ReactDOM.render(
-      React.createElement('tbody', null, ...trs),
-      document.getElementById('root')
-    )
-    setTimeout(loop, 1000)
   }
-  loop()
+  return array;
+}
+var start = init_table(10)
+console.log(start)
+  // trs.push(React.createElement('tr', null, ...tds))
+  // tds = []
+  // ReactDOM.render(
+  //   React.createElement('tbody', null, ...trs),
+  //   document.getElementById('root')
+  // )
+  // setTimeout(loop, 1000)
 };
 main()
