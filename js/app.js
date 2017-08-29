c7a3c5db4f7c2c22c502e6d8ca97c2566f81d07a
Mishin Ivan <mishin@n-core.ru>
2017-08-29 15:04:57 +0300

    [master] start work on live

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-ReactDOM.render(
-  <h1>hello world</h1>,
-  document.getElementById('root')
-);
+var main = function() {
+  var loop = function() {
+    var size = 50;
+    var tds = [];
+    var trs = [];
+    for (var j = 0; j < size; j++) {
+      for (var i = 0; i < size; i++) {
+        tds.push(React.createElement('td', {
+          style: {
+            backgroundColor: 'orange'
+          }
+        }, '.'))
+      }
+      trs.push(React.createElement('tr', null, ...tds))
+      tds = []
+    }
+    ReactDOM.render(
+      React.createElement('tbody', null, ...trs),
+      document.getElementById('root')
+    )
+    setTimeout(loop, 1000)
+  }
+  loop()
+};
+main()
