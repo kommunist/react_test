a70a5e9d35396adfbda10c63488edc3e44421f41
123@123.ru <123@123.ru>
2017-09-01 08:35:30 +0300

    [master] add board and table of states

--- js/live.js
+++ js/live.js
@@ -1,11 +1,13 @@
 var main = function() {
+  function concatKeys(first, second){
+    return String(first).concat(String(second))
+  }
 
   var Cell = React.createClass({
     getInitialState: function(){
       return { style: { backgroundColor: 'black' } }
     },
     changeColor: function(){
-      console.log(this.props.left)
       if (this.state.style.backgroundColor == 'black'){
         this.setState({ style: {backgroundColor: 'orange'} })
       }else{
@@ -21,6 +23,7 @@ var main = function() {
 
   var Board = React.createClass({
     getInitialState: function(){
+      var size = 10;
       var array = [];
       for (var j = 0; j < size; j++) {
         array[j] = []
@@ -28,34 +31,58 @@ var main = function() {
           array[j].push(0)
         }
       }
-    }
+      return {arr: array}
+    },
     render: function(){
-      
-    }
-  })
-
-  var init_table = function(size) {
-    var array = [];
-    for (var j = 0; j < size; j++) {
-      array[j] = []
-      for (var i = 0; i < size; i++) {
-        array[j].push(
-          <Cell left={array[j][array[j].length - 1]}/>
+      var trs = this.state.arr.map(function(arr_line, i){
+        var tds = []
+        tds = arr_line.map(function(td_arr, j){
+          var color = td_arr == 0 ? 'black' : 'orange'
+          return(
+            <td style = {{ backgroundColor: color }} key = {concatKeys(i, j)}></td>
+          )
+        })
+        return(
+          <tr key = {i}>
+            {
+              tds.map(function(a){
+                return a
+              })
+            }
+          </tr>
         )
-      }
+      })
+      return (
+        <tbody>
+          { trs.map(function(a){ return a }) }
+        </tbody>
+      )
     }
-    return array;
-  }
-
-  var trs = []
-  init_table(3).forEach(function(curr){
-    trs.push(
-      React.createElement('tr', null, ...curr)
-    )
   })
+
+  // var init_table = function(size) {
+  //   var array = [];
+  //   for (var j = 0; j < size; j++) {
+  //     array[j] = []
+  //     for (var i = 0; i < size; i++) {
+  //       array[j].push(
+  //         <Cell left={array[j][array[j].length - 1]}/>
+  //       )
+  //     }
+  //   }
+  //   return array;
+  // }
+
+  // var trs = []
+  // init_table(3).forEach(function(curr){
+  //   trs.push(
+  //     React.createElement('tr', null, ...curr)
+  //   )
+  // })
 
   ReactDOM.render(
-    React.createElement('tbody', null, ...trs),
+    <Board />,
+    // React.createElement('tbody', null, ...trs),
     document.getElementById('root')
   )
 }
