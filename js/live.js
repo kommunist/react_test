5fdcef210654f4eb45ff6fbc555b33e78b089cbb
Mishin Ivan <mishin@n-core.ru>
2017-09-04 15:03:22 +0300

    [master] add change state

--- js/live.js
+++ js/live.js
@@ -3,76 +3,68 @@ var main = function() {
     return String(first).concat(String(second))
   }
 
+  function numerToColor(numer){
+    return numer == 1 ? 'black' : 'orange'
+  }
+
   var Cell = React.createClass({
-    getInitialState: function(){
-      return { style: { backgroundColor: 'black' } }
+    manualChangeColor: function(){
+      this.props.manualChangeColorFunction()
     },
-    changeColor: function(){
-      if (this.state.style.backgroundColor == 'black'){
-        this.setState({ style: {backgroundColor: 'orange'} })
-      }else{
-        this.setState({ style: {backgroundColor: 'black'} })
-      }
-    },
-    handleCell: function(){
-      this.props.handleCell()
-    },
+
+    // changeColor: function(){
+    //   if (this.state.style.backgroundColor == 'black'){
+    //     this.setState({ style: {backgroundColor: 'orange'} })
+    //   }else{
+    //     this.setState({ style: {backgroundColor: 'black'} })
+    //   }
+    // },
+
+    //  как поменять цвет?
+
+
     render: function(){
       return (
-        <td style={{ backgroundColor: this.state.style.backgroundColor }} onClick = {this.handleCell} >!</td>
+        <td
+        style={{ backgroundColor: numerToColor(this.props.color) }}
+        onClick = {this.manualChangeColor} >!</td>
       )
     }
   })
 
   var Board = React.createClass({
+    manualChangeColor: function(first, second){
+      // здесь нужно изменить состояние матрицы
+    },
     getInitialState: function(){
       var size = 10;
       var array = [];
       for (var j = 0; j < size; j++) {
         array[j] = []
         for (var i = 0; i < size; i++) {
-          array[j].push(0)
+          array[j].push(Math.round(Math.random()))
         }
       }
       return {arr: array}
     },
-    handleCell: function(){
-      return console.log(this.state)
-    },
     render: function(){
-      var h = this.handleCell
+      var manualChangeColorFunction = this.manualChangeColor
       var trs = this.state.arr.map(function(arr_line, i){
         var tds = []
         tds = arr_line.map(function(td_arr, j){
-          var color = td_arr == 0 ? 'black' : 'orange'
-          return(
-            <Cell
-            key = {concatKeys(i, j)}
-            handleCell = { h }/>
-          )
+          return( <Cell
+                   key = { concatKeys(i, j) }
+                   color = { td_arr }
+                   manualChangeColorFunction = { manualChangeColorFunction }/> )
         })
-        return(
-          <tr key = {i}>
-            {
-              tds.map(function(a){
-                return a
-              })
-            }
-          </tr>
-        )
+        return( <tr key = {i}> { tds.map(function(a){ return a }) } </tr> )
       })
-      return (
-        <tbody>
-          { trs.map(function(a){ return a }) }
-        </tbody>
-      )
+      return ( <tbody> { trs.map(function(a){ return a }) } </tbody> )
     }
-
   })
 
   ReactDOM.render(
     <Board />,
-    // React.createElement('tbody', null, ...trs),
     document.getElementById('root')
   )
 }
